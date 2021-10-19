6f20eb141b051a31efbe7485958591392f0dec50
qiudb2021 <qiudb2022@outlook.com>
2021-10-19 16:59:16 +0800

    add game server config and simple test

---
+++ etc/runconfig.lua
@@ -0,0 +1,44 @@
+return {
+    -- 集群
+    cluster = {
+        node1 = "127.0.0.1:7001",
+        node2 = "127.0.0.2:7002"
+    },
+    agentmgr = {
+        node = "node1"
+    },
+    scene = {
+        node1 = {1001, 1002},
+        -- node2 = {1003}
+    },
+
+    --节点1
+    node1 = {
+        debug = {
+            port = 8100
+        },
+        gateway = {
+            [1] = {port = 8101},
+            [2] = {port = 8102}
+        },
+        login = {
+            [1] = {},
+            [2] = {}
+        }
+    },
+
+    --节点1
+    node2 = {
+        debug = {
+            port = 8200
+        },
+        gateway = {
+            [1] = {port = 8201},
+            [2] = {port = 8202}
+        },
+        login = {
+            [1] = {},
+            [2] = {}
+        }
+    }
+}
--- service/main.lua
+++ service/main.lua
@@ -1,6 +1,9 @@
 local skynet = require "skynet"
+local runconfig = requre "runconfig"
+
 skynet.start(function()
     -- 初始化
     skynet.error("[start main]")
+    skynet.error(runconfig.agentmgr.node)
     skynet.exit()
 end)
