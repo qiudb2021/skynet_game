00610c8445c3f94e8676c9f0ee75c796562782f2
qiudb2021 <qiudb2022@outlook.com>
2021-10-20 15:15:28 +0800

    gateway decode msg and forward to login

--- service/gateway/init.lua
+++ service/gateway/init.lua
@@ -29,12 +29,61 @@ local function disconnect(fd)
     -- body
 end
 
-local function process_msg(fd, msgstr)
-    skynet.error("gateway"..s.id.." recv from ["..fd.."] msgstr "..msgstr)
-end
-
+-- unpack msg
 -- 字符串协议格式，每条消息由“\r\n”作为结束符，
 -- 消息的各个参数用英文逗号分隔，第一个参数代表消息名称。
+local function str_unpack(msgstr)
+    skynet.error("gateway"..s.id.." recv from ["..fd.."] msgstr "..msgstr)
+    local msg = {}
+    while true do
+        local arg, rest = string.match( msgstr,"(.-),(.*)" )
+        if arg then
+            msgstr = rest
+            table.insert( msg, arg )
+        else
+            table.insert( msg, rest )
+            break
+        end
+    end
+    -- cmd, {cmd, ...}
+    return msg[1], msg
+end
+
+-- pack msg
+local function str_pack(cmd, msg)
+    return table.concat( msg, "," ).."\r\n"
+end
+
+local function random_login()
+    local node = skynet.getenv("node")
+    local nodecfg = runconfig[node]
+    local id = math.random( 1, #nodecfg.login )
+    return "login"..id
+end
+
+local function process_msg(fd, msgstr)
+    local cmd, msg = str_unpack(msgstr)
+    if not cmd then
+        skynet.error("gateway recv from ["..fd.."] not cmd.")
+        return
+    end
+
+    local conn = conns[fd]
+    local playerid = conn.playerid
+    -- 尚未完成登录
+    if not playerid then
+        local login = random_login()
+        skynet.error("todo require login to "..login.." auth username and password.")
+        -- skynet.send(login, "lua", "client", fd, cmd, msg)
+    -- 已完成登录
+    else
+        local gplayer = players[playerid]
+        local agent = gplayer.agent
+        skynet.error("todo forword msg to the agent.")
+        -- skynet.send(agent, "lua", "client", cmd, msg)
+    end
+end
+
 local function process_buff(fd, readbuff)
     while true do
         local msgstr, rest = string.match( readbuff, "(.-)\r\n(.*)" )
